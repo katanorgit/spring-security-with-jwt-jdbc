f303d00dbd4c7253823178555575852871cbd91e
Rajkumar Katanor <katanor.raj@gmail.com>
2022-12-05 20:40:43 -0600

    initial commit

---
+++ src/main/java/com/handson/demo/SpringSecurityWithJwtJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.handson.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJwtJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJwtJdbcApplication.class, args);
+	}
+
+}
